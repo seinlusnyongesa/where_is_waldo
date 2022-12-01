cd2c4c9489aff07cb7ed90915a634bd8f8c4fa80
seinlusnyongesa <seinlus044@gmail.com>
2022-12-01 10:58:25 -0800

    Change PlayersPopUp component styling

--- src/components/PlayersPopUp.js
+++ src/components/PlayersPopUp.js
@@ -3,10 +3,10 @@ import monk from "../assets/images/monk.jpg";
 import waldo from "../assets/images/waldo.jpg";
 const PlayersPopUp = () => {
   return (
-    <div className="absolute right-0">
-      <div className="flex flex-col gap-2  bg-slate-400 p-4 mx-8 mt-2 rounded-md">
-        <div className="flex gap-2 items-center">
-          <div className="w-20 ">
+    <div className="fixed right-0 z-10 top-16">
+      <div className="flex flex-col  bg-orange-400 mx-8 mt-2 rounded-md">
+        <div className="flex gap-2 items-center  hover:bg-orange-200 p-2">
+          <div className="w-14 ">
             <img src={monk} alt="monk" className="rounded-full" />
           </div>
           <div>
@@ -14,8 +14,8 @@ const PlayersPopUp = () => {
           </div>
         </div>
 
-        <div className="flex gap-2 items-center">
-          <div className="w-20 ">
+        <div className="flex  items-center hover:bg-orange-200 p-2">
+          <div className="w-14 ">
             <img src={waldo} alt="waldo" className="rounded-full" />
           </div>
           <div>
