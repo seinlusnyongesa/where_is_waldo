4ef796c322d2c13dc77f5c48ae688f6c79a523ba
seinlusnyongesa <seinlus044@gmail.com>
2022-12-01 10:57:20 -0800

    add Notification Component

---
+++ src/components/Notification.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+
+const Notification = ({ showNotification, setShowNotification }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowNotification({ show: false, charFound: false });
+    }, 3000);
+  }, [setShowNotification]);
+  function NotificationWithStyles() {
+    if (showNotification.charFound) {
+      return (
+        <div className="fixed top-14 left-[50%] bg-green-500 p-1 text-slate-200 text-xl">
+          you found waldo
+        </div>
+      );
+    }
+    return (
+      <div className="fixed top-14 left-[50%] bg-red-500 p-1 text-slate-100 text-xl">
+        that is not waldo
+      </div>
+    );
+  }
+
+  return <NotificationWithStyles />;
+};
+
+export default Notification;
