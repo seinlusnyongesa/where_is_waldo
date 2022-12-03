9e0d4997501e6e29c65d29c7863b818eff59aa18
seinlusnyongesa <seinlus044@gmail.com>
2022-12-03 02:51:16 -0800

    Add function to handle the target when clicked.
    lift gameData up the component tree

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
 import findwaldo from "../assets/images/findwaldo.jpg";
+import GameOver from "./GameOver";
+import MarkFound from "./MarkFound";
 import Notification from "./Notification";
 import Tagger from "./Tagger";
 
-const Game = () => {
+const Game = ({ gameData, findRemainChars }) => {
   const [pos, setPos] = useState({});
-  const [showTagger, setShowTagger] = useState(true);
+  const [showTagger, setShowTagger] = useState(false);
   const [showNofication, setShowNotification] = useState({
-    show: true,
+    show: false,
     charFound: false,
+    targetName: "",
   });
+  const [foundChars, setFoundChars] = useState([]);
+
+  const isGameOver = gameData.length === 0;
+
+  const handleTargetClick = (e) => {
+    const target = e.target.getAttribute("data-name");
+    const clickedTarget = gameData.find((j) => j.name === target);
+
+    let isTarget = clickedTarget.cordinate.find(
+      (j) => j.x === pos.x && j.y === pos.y
+    );
+
+    setShowNotification({
+      show: true,
+      charFound: !!isTarget,
+      targetName: target,
+    });
+    if (!!isTarget) {
+      setFoundChars([...foundChars, { name: target, cordinate: pos }]);
+      findRemainChars(target);
+    }
+  };
 
   const handleClick = (e) => {
     setPos({
@@ -26,13 +51,23 @@ const Game = () => {
     <>
       <div className="bg-gray-700 relative " onClick={handleClick}>
         <img src={findwaldo} alt="find waldo" className="pt-8 " />
-        {showTagger ? <Tagger pos={pos} /> : ""}
+        {showTagger ? (
+          <Tagger
+            pos={pos}
+            handleTargetClick={handleTargetClick}
+            gameData={gameData}
+          />
+        ) : (
+          ""
+        )}
         {showNofication.show && (
           <Notification
             setShowNotification={setShowNotification}
             showNotification={showNofication}
           />
         )}
+        {foundChars.length > 0 && <MarkFound foundChars={foundChars} />}
+        {isGameOver && <GameOver />}
       </div>
     </>
   );
