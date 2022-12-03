9289f1ad25468ffaedf9d0b282a244ae1ef7481d
seinlusnyongesa <seinlus044@gmail.com>
2022-12-03 02:46:13 -0800

    Add json data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Game from "./components/Game";
 import Navbar from "./components/Navbar";
 import PlayersPopUp from "./components/PlayersPopUp";
 
+import data from "./data.json";
+
 function App() {
-  let [isPlayerPopupOpen, setIsPlayerPopupOpen] = useState(true);
+  let [isPlayerPopupOpen, setIsPlayerPopupOpen] = useState(false);
+  const [gameData, setGameData] = useState([]);
+  useEffect(() => {
+    setGameData(data.data);
+  }, []);
+
+  const findRemainChars = (name) => {
+    const remainChars = gameData.filter((v) => v.name !== name);
+    setGameData(remainChars);
+  };
   return (
     <>
       <Navbar
         setPlayerPopup={setIsPlayerPopupOpen}
         isPlayerPopupOpen={isPlayerPopupOpen}
+        gameData={gameData}
       />
       {isPlayerPopupOpen ? <PlayersPopUp /> : ""}
-      <Game />
+      <Game gameData={gameData} findRemainChars={findRemainChars} />
     </>
   );
 }
