546578969ee9fb670e8260d713081cd470c52801
seinlusnyongesa <seinlus044@gmail.com>
2022-12-03 02:44:22 -0800

    show remaining Characters to be found and remove timer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Navbar = ({ setPlayerPopup, isPlayerPopupOpen }) => {
+const Navbar = ({ setPlayerPopup, isPlayerPopupOpen, gameData }) => {
   return (
     <nav className="bg-slate-500 fixed w-full z-10">
-      <div className="flex justify-around  items-center p-2">
+      <div className="flex justify-between mx-12  items-center p-2">
         <h1 className="text-2xl">where's weldo?</h1>
-        <h1 className="text-2xl">00:00:00</h1>
+
         <button
           onClick={() => setPlayerPopup(!isPlayerPopupOpen)}
           className="bg-red-200 rounded-full p-1 w-8 h-8 text-center font-bold"
         >
-          3
+          {gameData ? gameData.length : 0}
         </button>
       </div>
     </nav>
