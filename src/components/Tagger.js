d42cb1dfe36178d422bba8fe883b721173528ad5
seinlusnyongesa <seinlus044@gmail.com>
2022-12-03 02:42:01 -0800

    Auto populate targer elements by mapping through game data

--- src/components/Tagger.js
+++ src/components/Tagger.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const Tagger = ({ pos }) => {
+const Tagger = ({ pos, handleTargetClick, gameData }) => {
+  console.log(gameData);
   return (
     <ul
       className="absolute bg-orange-600 text-lg font-semibold cursor-pointer rounded-md"
       style={{ left: `${pos.x}%`, top: `${pos.y}%` }}
     >
-      <li className="hover:bg-orange-200 p-2 rounded-md">waldo</li>
-      <li className="hover:bg-orange-200 p-2 rounded-md">monk</li>
+      {gameData.map((p, i) => (
+        <li
+          key={i}
+          className="hover:bg-orange-200 p-2 rounded-md"
+          data-name={`${p.name}`}
+          onClick={handleTargetClick}
+        >
+          {p.name}
+        </li>
+      ))}
     </ul>
   );
 };
